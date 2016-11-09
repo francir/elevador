14f7b0a59787ef9859ca4cadc95bfb25fb598bda
Francir Silvério <Francir Silvrio>
2016-11-08 23:07:29 -0200

    Initial

---
+++ ElevadorFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevador
+{
+    public class ElevadorFactory
+    {
+        public static IElevador Create()
+        {
+            return new Elevador(10, 8);
+        }
+    }
+}
